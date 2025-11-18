09f05ec2e4a78a774f2ae3d149f1479a68662b22
dottedboxguy <loplop7levrai@gmail.com>
2025-11-18 09:54:51 +0100

    implemented HiddenExit

--- game/src/main/java/terminal/adventure/game/HiddenExit.java
+++ game/src/main/java/terminal/adventure/game/HiddenExit.java
@@ -4,9 +4,13 @@ public class HiddenExit extends Exit {
 
 	private boolean isHidden;
 
+	public HiddenExit(Location dest){
+		super(dest);
+		this.isHidden = true;
+	}
+
 	public void unveil() {
-		// TODO - implement HiddenExit.unveil
-		throw new UnsupportedOperationException();
+		this.isHidden = false;
 	}
 
 }
