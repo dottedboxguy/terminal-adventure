c7da4c2ca1bc2e13218a85eda065753461fc301a
Petit-Pelican <thomas@calberac.fr>
2025-11-18 10:30:06 +0100

    beggining of Item implementation

--- game/src/main/java/terminal/adventure/game/Item.java
+++ game/src/main/java/terminal/adventure/game/Item.java
@@ -1,11 +1,23 @@
 package terminal.adventure.game;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.Map;
 
 public abstract class Item {
-
+	public final String NAME;
 	Collection<Spell> spells;
-	private Collection stats;
-	private String description;
+	private Map<String,Integer> stats;
+	
+
+	public Item(String name) {
+		this.NAME = name;
+		this.spells = null;
+		this.stats = null;
+	}
+	public Item() {
+		this.NAME = null;
+		this.spells = null;
+		this.stats = null;
+	}
 
 }
