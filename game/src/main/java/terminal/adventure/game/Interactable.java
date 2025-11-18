3a6bbd37cc77390ba9e4bebdaa259b2db8f90c54
dottedboxguy <loplop7levrai@gmail.com>
2025-11-18 10:04:09 +0100

    implemented Interactable

--- game/src/main/java/terminal/adventure/game/Interactable.java
+++ game/src/main/java/terminal/adventure/game/Interactable.java
@@ -2,8 +2,13 @@ package terminal.adventure.game;
 
 public abstract class Interactable {
 
-	private String description;
-	private String name;
+	public final String description;
+	public final String name;
+
+	public Interactable(String desc, String name){
+		this.description = desc;
+		this.name = name;
+	}
 
 	public abstract void action();
 
