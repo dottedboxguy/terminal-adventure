f063bdbd3f8781c82178f7339be32e6d80f6902f
pjouet01 <paul.jouet@etu.univ-poitiers.fr>
2025-11-29 17:31:45 +0100

    update character with slots

--- game/src/main/java/terminal/adventure/game/Character.java
+++ game/src/main/java/terminal/adventure/game/Character.java
@@ -1,3 +1,5 @@
+package terminal.adventure.game;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -6,14 +8,20 @@ public class Character {
     private List<Stat> baseStats;
     private List<Item> equippedItems;
 
+    private List<Slot> inventory;
+    
+    
+    
     public Character(String name) {
         this.name = name;
         this.baseStats = new ArrayList<>();
-        this.equippedItems = new ArrayList<>();
+        this.inventory = new ArrayList<>();
+        
+        
     }
 
-    public void addBaseStat(Stat stat) {
-        baseStats.add(stat);
+    public void addBaseStat(String name, int value) {
+        baseStats.add(new Stat(name, value));
     }
 
     public void equipItem(Item item) {
@@ -42,5 +50,24 @@ public class Character {
 
     // Getters
     public String getName() { return name; }
+    
+    public int getTotalStat(String name) {
+    	int res = 0;
+    	
+    	// Base Stats
+    	for ( Stat stat : this.baseStats) { 
+    		if (stat.getName().equals(name)) {
+    			res += stat.getValue();
+    		}
+    	}
+    	
+    	for( Slot slot : this.inventory ) {
+    		res += slot.getStat(name);
+    	}
+    	
+    	return res;
+    }
+   
+    
     public List<Item> getEquippedItems() { return equippedItems; }
 }
