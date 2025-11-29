38bffa9f0370e9fbfbb77cf70438e68dd20d6379
Petit-Pelican <thomas@calberac.fr>
2025-11-29 16:40:26 +0100

    bug in AIcontroller fixed

--- game/src/main/java/terminal/adventure/game/AIController.java
+++ game/src/main/java/terminal/adventure/game/AIController.java
@@ -1,6 +1,6 @@
 package terminal.adventure.game;
 
-public class IAController extends CharacterController {
+public class AIController extends CharacterController {
 
 	public Command SelectAction() {
 		// TODO - implement IAController.SelectAction
