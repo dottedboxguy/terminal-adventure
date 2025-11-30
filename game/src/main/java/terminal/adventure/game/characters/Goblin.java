fa42e018befc1b7a38dd555e1ca9b749b12b3384
dottedboxguy <loup.pigeaud.chesse@etu.univ-poitiers.fr>
2025-11-30 02:57:41 +0100

    added descriptions to constructors

--- game/src/main/java/terminal/adventure/game/characters/Goblin.java
+++ game/src/main/java/terminal/adventure/game/characters/Goblin.java
@@ -2,8 +2,8 @@ package terminal.adventure.game.characters;
 
 public class Goblin extends Character{
     
-    public Goblin(String name){
-        super(name);
+    public Goblin(String name, String description){
+        super(name, description);
         this.baseStats.setMaxHealth(40);
         this.baseStats.setCurrentHealth(40);
         this.baseStats.setStrength(3);
--- game/src/main/java/terminal/adventure/game/characters/Human.java
+++ game/src/main/java/terminal/adventure/game/characters/Human.java
@@ -2,8 +2,8 @@ package terminal.adventure.game.characters;
 
 public class Human extends Character {
 
-    public Human(String name){
-        super(name);
+    public Human(String name, String description){
+        super(name, description);
         this.baseStats.setMaxHealth(100);
         this.baseStats.setCurrentHealth(100);
         this.baseStats.setStrength(5);
