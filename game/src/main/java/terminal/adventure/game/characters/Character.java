1ae328700208d7dbd89d80df5dd3f6f65e0aa095
Petit-Pelican <thomas@calberac.fr>
2025-11-30 02:37:29 +0100

    getter and import added

--- game/src/main/java/terminal/adventure/game/characters/Character.java
+++ game/src/main/java/terminal/adventure/game/characters/Character.java
@@ -1,24 +1,32 @@
 package terminal.adventure.game.characters;
 
+import exceptions.noPossibilitesException;
+import exceptions.tooManyPossibilitesException;
+import terminal.adventure.game.Location;
+import terminal.adventure.game.Lookable;
 import terminal.adventure.game.Stats;
 import terminal.adventure.game.inventory.Equipment;
 import terminal.adventure.game.inventory.Inventory;
 import terminal.adventure.game.inventory.items.Item;
-public class Character {
+
+public abstract class Character implements Lookable{
     
     public final String NAME;
+    public final String DESCRIPTION;
     protected Inventory inventory;
     protected Equipment equipment;
     protected Stats baseStats;
+    private Location currentLocation;
     
     
-    public Character(String name) {
+    public Character(String name, String description) {
         this.NAME = name;
+        this.DESCRIPTION = description;
         this.inventory = new Inventory();
         this.baseStats = new Stats();
     }
 
-    public void equipItem(Item item) {
+    public void equipItem(Item item) throws noPossibilitesException, tooManyPossibilitesException {
         this.equipment.equip(item);
     }
 
@@ -28,4 +36,10 @@ public class Character {
     public int getTotalStat(String name) {
         // TO DO
     }
+
+    @Override
+    public String getDescription() {
+        return this.DESCRIPTION;
+    }
+
 }
