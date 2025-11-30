6f1bc270f9207a98f739a657c84aa6e51a891210
Petit-Pelican <thomas@calberac.fr>
2025-11-30 18:32:14 +0100

    chest modified

--- game/src/main/java/terminal/adventure/game/interactables/InteractableChest.java
+++ game/src/main/java/terminal/adventure/game/interactables/InteractableChest.java
@@ -1,9 +1,8 @@
 package terminal.adventure.game.interactables;
 
-import exceptions.CannotAccessContentException;
 import terminal.adventure.game.inventory.Inventory;
 
-public class InteractableChest extends InteractableWithInventory{
+public class InteractableChest extends Interactable{
 
     private boolean isOpened = false;
 	public InteractableChest(String description, String name, Inventory inventory){
@@ -14,15 +13,5 @@ public class InteractableChest extends InteractableWithInventory{
 	public void action(){
         this.isOpened = true;
     };
-
-    @Override
-    public Inventory getInventory() throws CannotAccessContentException{
-        if (isOpened){
-            return this.inventory;
-        }
-        else{
-            throw new CannotAccessContentException("Cannot Interact with the chest...\n It is closed!");
-        }
-    }
 
 }
