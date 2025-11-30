4cd1a3fe0e7ad9e0d30c0411e0ad433622a46352
Petit-Pelican <thomas@calberac.fr>
2025-11-30 03:39:36 +0100

    getDescription added

--- game/src/main/java/terminal/adventure/game/exits/Exit.java
+++ game/src/main/java/terminal/adventure/game/exits/Exit.java
@@ -1,13 +1,16 @@
 package terminal.adventure.game.exits;
 import terminal.adventure.game.Location;
+import terminal.adventure.game.Lookable;
 
-public abstract class Exit {
+public abstract class Exit implements Lookable{
     protected Location destination;
     protected final String NAME;
+    private final String DESCRIPTION;
 
-    public Exit(Location destination, String name) {
+    public Exit(Location destination, String name, String description) {
         this.destination = destination;
         this.NAME = name;
+        this.DESCRIPTION = description;
     }
 
     public Location getDestination() {
@@ -19,6 +22,11 @@ public abstract class Exit {
 
     // Message to display if crossing fails
     public abstract String getFailMessage();
+
+    @Override
+    public String getDescription(){
+        return this.DESCRIPTION;
+    }
 
     public String getName(){ return this.NAME;}
 }
