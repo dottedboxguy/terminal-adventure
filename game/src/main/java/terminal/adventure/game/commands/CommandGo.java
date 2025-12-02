1981722eb1c4bf4c2d74bbe2ad62df13e90c6108
Petit-Pelican <thomas@calberac.fr>
2025-12-03 00:53:46 +0100

    Command go implemented

--- game/src/main/java/terminal/adventure/game/commands/CommandGo.java
+++ game/src/main/java/terminal/adventure/game/commands/CommandGo.java
@@ -1,16 +1,64 @@
+
 package terminal.adventure.game.commands;
 
+import java.util.Collection;
+
 import terminal.adventure.game.Console;
+import terminal.adventure.game.Location;
+import terminal.adventure.game.exits.Exit;
+
 
 public class CommandGo implements Command {
-
+    
     @Override
     public void execute(String[] args, Console console) {
+
+        if (args.length == 0) {
+            System.out.println("Where do you wanna GO ?");
+            System.out.println(help());
+            System.out.println("\nAvailable locations from here :");
+            displayAvailableExits(console.getPlayer().getActor().getCurrentLocation());
+            return;
+        }
+        
+        Location currentLocation = console.getPlayer().getActor().getCurrentLocation();
+        Exit exit = currentLocation.getExit(args[0]);
+        
+        if (exit == null) {
+            System.out.println("there is no location named \"" + String.join(" ", args) + "\" near.");
+            System.out.println("\nAvailable locations from here :");
+            displayAvailableExits(console.getPlayer().getActor().getCurrentLocation());
+            return;
+        }
+        
+        if (!exit.canCross()) {
+            System.out.println(exit.getFailMessage());
+            return;
+        }
+        
+        Location newLocation = exit.getDestination();
+        console.getPlayer().getActor().setCurrentLocation(newLocation);
+        
+        System.out.println("You walk through" + exit.getName() + ".");
+        System.out.println("You've arrrived at : " + newLocation.getName());
+        System.out.println(newLocation.getDescription());
+    }
+    
+    private void displayAvailableExits(Location location) {
+        if (location.getVisibleExits().isEmpty()) {
+            System.out.println("There's no exit that you can see. You're locked in.");
+        } else {
+            Collection<Exit> destinations = location.getVisibleExits().values();
+            for (Exit destination : destinations) {
+                System.out.println(" - " + destination.getDestination().getName() + " :");
+                System.out.println(destination.getDestination().getDescription());
+            }
+        }
         
     }
-
+    
     @Override
     public String help() {
-        return "\n Usage : \n GO \t <Location>";
+        return "GO <Location's name>\n you try to go to that location if an exit allows you to do so.\n";
     }
 }
