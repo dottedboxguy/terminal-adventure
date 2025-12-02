82b880cfee8774ef1eb77d184d5dcefc6049957d
Pajul7 <cyberp.lejoueur@gmail.com>
2025-12-02 01:03:57 +0100

    Preparing grounds for AIController to randomly move across the map each turn

--- game/src/main/java/terminal/adventure/game/controllers/AIController.java
+++ game/src/main/java/terminal/adventure/game/controllers/AIController.java
@@ -1,14 +1,17 @@
 package terminal.adventure.game.controllers;
 
 import java.util.List;
+import java.util.Random;
 
-import terminal.adventure.game.Fight;
 import terminal.adventure.game.actors.Actor;
+import terminal.adventure.game.exits.Exit;
 import terminal.adventure.game.inventory.items.Item;
 import terminal.adventure.game.inventory.slots.Slot;
 
 public class AIController extends Controller {
 
+	Random random = new Random();
+	
 	public AIController(Actor actor, Faction faction) {
 		super(actor, faction);
 	}
@@ -29,10 +32,28 @@ public class AIController extends Controller {
 	@Override
 	public void play() {
 						
-		Actor target = this.actor.getFight().getFightersByAntiFaction(this.getFaction()).get(0);
+		if (this.actor.getFight() != null) {
+
+			Actor target = this.actor.getFight().getFightersByAntiFaction(this.getFaction()).get(0);
+			
+			this.actor.attack(target);
+		} else {
 		
-		this.actor.attack(target);
-		
-		
+			List<Exit> exits = this.actor.getCurrentLocation().getAllExits();
+			
+			
+			int nExits = exits.size();
+			if ( nExits > 0) {
+				
+				int e = random.nextInt( nExits );
+				
+				//this.actor.move(exits[e});
+				
+			}
+			
+			
+			
+			
+		}
 	}
 }
