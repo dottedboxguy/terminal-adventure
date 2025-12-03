b72809ccd2816c95fa2b6dd39013084f3143cbbc
Pajul7 <cyberp.lejoueur@gmail.com>
2025-12-03 23:51:46 +0100

    Location implements Storage

--- game/src/main/java/terminal/adventure/game/Location.java
+++ game/src/main/java/terminal/adventure/game/Location.java
@@ -9,9 +9,10 @@ import terminal.adventure.game.exits.Exit;
 import terminal.adventure.game.exits.HiddenExit;
 import terminal.adventure.game.interactables.Interactable;
 import terminal.adventure.game.inventory.Inventory;
+import terminal.adventure.game.inventory.Storage;
 import terminal.adventure.game.inventory.items.Item;
 
-public class Location implements Lookable{
+public class Location implements Lookable, Storage{
     private final String NAME;
     private final String DESCRIPTION;
 	private List<Actor> actors;
@@ -88,7 +89,31 @@ public class Location implements Lookable{
 	}
 
     public String getName() { return this.NAME; }
-	public Inventory getInventory() { return this.inventory; }
-	@Override
+
+    @Override
     public String getDescription() { return ""; }
+
+	
+	//------------------- Storage Methods -------------
+	
+	@Override
+	public void addItem(Item item) {
+		this.inventory.add(item);
+		
+	}
+
+	@Override
+	public void removeItem(Item item) {
+		this.inventory.add(item);
+	}
+
+	@Override
+	public List<Item> getItems() {
+		return this.inventory.getItems();
+	}
+
+	@Override
+	public List<Item> searchItems(String itemName) {
+		return this.inventory.searchItemsByName(itemName);
+	}
 }
