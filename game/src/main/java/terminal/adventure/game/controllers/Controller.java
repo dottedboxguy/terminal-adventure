68a285879aa70a63bd964f1f15156455618eb82b
Petit-Pelican <thomas@calberac.fr>
2025-12-03 22:01:32 +0100

    controller beggining of modifications

--- game/src/main/java/terminal/adventure/game/controllers/Controller.java
+++ game/src/main/java/terminal/adventure/game/controllers/Controller.java
@@ -1,28 +1,46 @@
 package terminal.adventure.game.controllers;
 
+import java.util.HashMap;
+
 import terminal.adventure.exceptions.tooManyPossibilitesException;
 
 import java.util.List;
+import java.util.Map;
 
 import terminal.adventure.game.actors.Actor;
+import terminal.adventure.game.commands.Command;
 import terminal.adventure.game.inventory.items.Item;
 import terminal.adventure.game.inventory.slots.Slot;
 
 public abstract class Controller {
 
     protected Actor actor;
-
     private Faction faction;
-        
+    private final Map<String, Command> commands = new HashMap<>();
+	
     public Controller(Actor actor, Faction faction) {
     	this.actor = actor;
     	this.faction = faction;
+		registerCommands();
     }
     
     public Controller(Faction faction) {
     	this.faction = faction;
+		registerCommands();
     }
-	
+
+	private void registerCommands() {
+    	commands.put("GO", new CommandGo());
+        commands.put("QUIT", new CommandQuit());
+        commands.put("HELP", new CommandHelp());
+        commands.put("LOOK", new CommandLook());
+        commands.put("TAKE", new CommandTake());
+        commands.put("USE", new CommandUse());
+    }
+
+    public Map<String, Command> getCommands() {
+    	return this.commands;
+    }
     
     
     // -------------- Inventory-related methods ----------------
