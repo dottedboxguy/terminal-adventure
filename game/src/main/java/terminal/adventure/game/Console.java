077666b905c0ec6ba5ca42692a47d0e7e6620205
dottedboxguy <loplop7levrai@gmail.com>
2025-12-05 15:22:06 +0100

    renamed attack to fight

--- game/src/main/java/terminal/adventure/game/Console.java
+++ game/src/main/java/terminal/adventure/game/Console.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 import java.util.function.Function;
 
 import terminal.adventure.game.commands.Command;
-import terminal.adventure.game.commands.CommandAttack;
+import terminal.adventure.game.commands.CommandFight;
 import terminal.adventure.game.commands.CommandCast;
 import terminal.adventure.game.commands.CommandEquip;
 import terminal.adventure.game.commands.CommandGo;
@@ -43,7 +43,7 @@ public class Console{
         commands.put("USE" , CommandUse::new);
         commands.put("CAST", CommandCast::new);
         commands.put("EQUIP", CommandEquip::new);
-        commands.put("ATTACK", CommandAttack::new);
+        commands.put("FIGHT", CommandFight::new);
     }
 
     public Map<String, Function<String[], Command>> getCommands() {
